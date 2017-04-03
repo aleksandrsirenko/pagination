1aef776722a0d021e75c98b3fcbaabe8d636a6e5
Aleksandr <sirenko.alexandr.l@gmail.com>
2017-04-03 20:32:23 +0000

    added script

--- js/index.js
+++ js/index.js
@@ -1,18 +1,27 @@
-var taskService = (function () {
-     return {
-         baseUrl: 'https://jsonplaceholder.typicode.com/photos/',
-         getTasks: function () {
-             var self = this;           
-            $.ajax({
-                 url: self.baseUrl,
-                 type: 'GET',
-                 success: function (response) { // response is an array of objects                   
-                     console.log('Success', response);
-                     },
-                 error: function (response) {
-                     console.log('Error', response);
-                 }
-             })
-         }
-     }
- })(); 
+var arr = [];
+function getPhotos() {
+  $.ajax({
+  url: 'https://jsonplaceholder.typicode.com/photos/',
+type: 'GET',
+success: function(data) {
+    console.log(data);
+arr = data.slice(0,5);
+$('img').attr('src', arr[0].url);
+    
+},
+error: function(data) {
+}
+})
+}
+
+getPhotos();
+
+
+$('.pagination li:first').addClass('active');
+$('.pagination li').click(function() {
+            $('.pagination li').removeClass('active');
+            $(this).addClass('active');
+            var page = $(this).find('a').text();
+            var url = arr[page-1].url;
+            $('img').attr('src', url);
+});
